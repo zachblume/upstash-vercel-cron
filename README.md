4dc5065ca9e9c56620547751ab03ee6fd27f9463
Zach Blume <zachblume@gmail.com>
2023-11-26 13:35:06 -0500

    Example upstash + vercel script and readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # upstash-vercel-cron
+
+Vercel has a nice Cron job syntax where you can list api directory endpoints to be called periodically in `vercel.json`. Because Vercel keeps every past deployment hosted, as well as generated new ones for each branch, they only implement this in production to avoid having thousands of past deployments running cron.
+
+However, it's a common requirement to have a staging environment that matches production as closely as possible, including cron jobs. My setup on Vercel isolates a branch called `staging`  and has a custom domain mapped to `myapp-staging.vercel.app`, since I also have an isolated staging database. I do this because branches auto-deploy to preview on Vercel, so the preview database isn't gaurenteed to be in a clean state that matches the migration history of staging, because I also auto-deploy database migrations with the builds.
+
+This repo is a workaround for the staging-production difference, with a simple node script that runs on deployment to staging, and reconciles QStash to execute the same cron job schedule as specified in `vercel.json `... to make sure that the reconciliation is complete, it just deletes all existing jobs and recreates them according to the new `vercel.json`, which is not an ideal solution if you're using Qstash schedules for anything else!
+
+## Usage
+
+You'll need two environment variables set for your staging environment:
+- `UPSTASH_URL` - the url of your upstash redis instance
+- `UPSTASH_TOKEN` - the auth token for QStash
+- `ENVIRONMENT` - needs to be set to "staging" in the environment you want to trigger this script in (or you can rewrite the conditional)
+- `STAGING_CUSTOM_URL` - the custom url for your staging environment, so the script knows the callback prefix to pass to QStash
+
+Then, add the following line to your package.json `vercel-build` script, like so:
+```
+{
+    "name": "myapp",
+    ...
+    "scripts": {
+        ...
+        "vercel-build": "node putCronsOnStaging.mjs && next build",
+        ...
+    },
+    ...
+}
+```
---
+++ putCronsOnStaging.mjs
@@ -0,0 +1,78 @@
+import fs from "fs";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Only run the script if we're deploying to staging
+if (process.env.ENVIRONMENT === "staging") {
+    console.log("Scheduling cron jobs on staging");
+    destroyAndScheduleAllCronJobs();
+}
+
+async function fetchCurrentSchedules() {
+    const response = await fetch("https://qstash.upstash.io/v1/schedules", {
+        headers: {
+            Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error fetching schedules: ${response.statusText}`);
+    }
+
+    return response.json();
+}
+
+async function destroyAllSchedules() {
+    const schedules = await fetchCurrentSchedules();
+    const promises = schedules.map(async (schedule) => {
+        const response = await fetch(`https://qstash.upstash.io/v1/schedules/${schedule.id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error destroying schedule ${schedule.id}: ${response.statusText}`);
+        }
+    });
+
+    await Promise.allSettled(promises);
+}
+
+async function scheduleSingleCronJob(job) {
+    const url = process.env.QSTASH_URL + encodeURIComponent(job.endpoint);
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${process.env.QSTASH_TOKEN}`,
+            "Content-Type": "application/json",
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error scheduling job for endpoint ${job.endpoint}: ${response.statusText}`);
+    }
+
+    console.log(`Scheduled cron job for endpoint ${job.endpoint}`);
+}
+
+async function destroyAndScheduleAllCronJobs() {
+    await destroyAllSchedules();
+
+    // Read the vercel.json file to get the cron jobs
+    const vercelConfig = JSON.parse(fs.readFileSync("vercel.json", "utf8"));
+    const cronJobs = vercelConfig.crons || [];
+
+    // Schedule the jobs in parallel
+    const promises = cronJobs.map((job) =>
+        scheduleSingleCronJob({
+            ...job,
+            // Add the staging custom URL to the endpoint to make it an absolute URL
+            endpoint: `${process.env.STAGING_CUSTOM_URL}${job.endpoint}`,
+        })
+    );
+    await Promise.allSettled(promises);
+}
